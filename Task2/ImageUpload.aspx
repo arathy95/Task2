3e4ea5716d2b9c642ac942ef614b358710218f79
Arathy Pillai <pillai.arathy95@hotmail.com>
2019-09-05 11:59:20 +0530

    Add project files.

---
+++ Task2/ImageUpload.aspx
@@ -0,0 +1,26 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="ImageUpload.aspx.cs" Inherits="Task2.ImageUpload" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <table>
+                <%--<tr>
+                    <td>Image: </td><td><asp:Image ID="imageshow" runat="server" Width="150px" /></td>
+                </tr>--%>
+                <tr>
+                    <td>Upload Image: </td><td><asp:FileUpload ID="uploadimage" runat="server" /></td>
+                </tr>
+                <tr>
+                    <td><asp:Button ID="btnupload" runat="server" Text="UPLOAD IMAGE" OnClick="btnupload_Click" /></td>
+                </tr>
+            </table>
+        </div>
+    </form>
+</body>
+</html>
---
+++ Task2/ImageUpload.aspx.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.IO;
+using System.Data.SqlClient;
+
+namespace Task2
+{
+    public partial class ImageUpload : System.Web.UI.Page
+    {
+        SqlConnection con = new SqlConnection(@"Data Source=LAPTOP-D2RS25A9\SQLEXPRESS;Initial Catalog=ImageUpload;Integrated Security=True");
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnupload_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                //byte[] imgdata = uploadimage.FileBytes;
+
+                string dirpath = Server.MapPath("images");
+                if(Directory.Exists(dirpath))
+                {
+                    Directory.CreateDirectory(dirpath);
+                }
+                string filepath = dirpath + "\\" + uploadimage.FileName;
+                uploadimage.SaveAs(filepath);
+
+                string url = "~/images/" + uploadimage.FileName;
+
+                
+                SqlCommand cmd = new SqlCommand("insert into tbl_image (imageurl) values (@imageurl)", con);
+                con.Open();
+                cmd.Parameters.AddWithValue("@imageurl", url);
+                
+                cmd.ExecuteNonQuery();
+                con.Close();
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+
+            
+        }
+    }
+}
